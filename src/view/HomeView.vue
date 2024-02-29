70c7fd13718e37eb93ca5a831ff77f14943274ce
KitsuC <83906764+KitsuC@users.noreply.github.com>
2024-02-29 17:26:56 +0700

    banner

--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -1,22 +1,224 @@
 <template>
-  <div class="banner-wrap ">
-    <h2 class="text-[96px] text-white relative text-center top-[32px]">LION COIN</h2>
-    <div class="">
-      <img src="../assets/images/Lionbaner.png" alt="" class="absolute bottom-[168px] left-[-18%]">
-
-    </div>
-    <div class="flex flex-col gap-[4px] max-w-[192px] w-full justify-self-end">
-      <div class="flex gap-[4px] justify-self-start">
-        <img class="w-[32px] h-[32px]" src="../assets/images/coin-1.png" alt="">
-        <img class="w-[32px] h-[32px]" src="../assets/images/coin-2.png" alt="">
-        <img class="w-[32px] h-[32px]" src="../assets/images/coin-3.png" alt="">
-        <img class="w-[32px] h-[32px]" src="../assets/images/coin-4.png" alt="">
+  <div>
+    <div class="banner-wrap ">
+      <h2
+          class="text-[96px] text-white relative z-10 text-center min-[992px]:text-[150px] min-[992px]:w-fit min-[992px]:self-end
+                min-[992px]:right-[85px] min-[1200px]:text-[220px]"
+      >
+        LION COIN
+      </h2>
+      <div class="">
+        <img
+            class="absolute bottom-[168px] left-[-53px] w-[370px] min-[576px]:bottom-[120px] min-[768px]:bottom-0
+                 min-[768px]:w-[646px] min-[992px]:w-[950px] min-[1200px]:w-[1052px] min-[1200px]:left-[-5px] "
+            src="../assets/images/Lionbaner.png"
+            alt=""
+        >
+        <p
+            class="absolute top-[31%] left-[80px] text-[24px] font-[900] text-white min-[576px]:top-[38%] min-[768px]:top-[36%]
+                 min-[768px]:text-[32px] min-[768px]:left-[260px] min-[992px]:left-[544px] min-[992px]:top-[44%]"
+        >
+          You guys buy them? So pathetic
+        </p>
+        <img class="absolute h-[46px] w-[94px] top-[37%] left-[120px] min-[576px]:top-[45%] min-[768px]:w-[214px]
+                  min-[768px]:left-[195px] min-[768px]:h-[106px] min-[992px]:top-[52%] min-[992px]:left-[410px]"
+             src="../assets/images/vector-white.png"
+             alt="">
       </div>
-      <div class="flex gap-[4px] justify-self-end">
-        <img class="w-[32px] h-[32px]" src="../assets/images/coin-5.png" alt="">
-        <img class="w-[32px] h-[32px]" src="../assets/images/coin-6.png" alt="">
-        <img class="w-[32px] h-[32px]" src="../assets/images/coin-7.png" alt="">
-        <img class="w-[32px] h-[32px]" src="../assets/images/coin-8.png" alt="">
+      <div
+          class="z-10 flex flex-col justify-end min-[768px]:flex-row-reverse min-[768px]:px-[20px] min-[768px]:pb-[30px]
+              min-[576px]:justify-between min-[768px]:gap-[40px] min-[992px]:pb-[64px] min-[992px]:px-[80px] "
+      >
+        <div
+            class="flex flex-col self-center justify-center gap-[8px] max-w-[192px] w-full max-[768px]:mb-[12px]
+          min-[768px]:max-w-[217px] max-[768px]:self-end max-[768px]:mr-[16px] min-[768px]:gap-[16px] min-[768px]:h-full
+           min-[992px]:max-w-[375px] min-[1200px]:max-w-[504px] min-[1200px]:gap-[32px] flex-shrink-0"
+        >
+          <div class="flex gap-[8px] self-start min-[768px]:gap-[10px] min-[992px]:gap-[30px] min-[1200px]:gap-[48px]">
+            <img class="w-[32px] aspect-square min-[768px]:w-[40px] min-[992px]:w-[60px] min-[1200px]:w-[80px]"
+                 src="../assets/images/coin-1.png" alt="">
+            <img class="w-[32px] aspect-square min-[768px]:w-[40px] min-[992px]:w-[60px] min-[1200px]:w-[80px]"
+                 src="../assets/images/coin-2.png" alt="">
+            <img class="w-[32px] aspect-square min-[768px]:w-[40px] min-[992px]:w-[60px] min-[1200px]:w-[80px]"
+                 src="../assets/images/coin-3.png" alt="">
+            <img class="w-[32px] aspect-square min-[768px]:w-[40px] min-[992px]:w-[60px] min-[1200px]:w-[80px]"
+                 src="../assets/images/coin-4.png" alt="">
+          </div>
+          <div class="flex gap-[8px] self-end min-[768px]:gap-[10px] min-[992px]:gap-[30px] min-[1200px]:gap-[48px]">
+            <img class="w-[32px] aspect-square min-[768px]:w-[40px] min-[992px]:w-[60px] min-[1200px]:w-[80px]"
+                 src="../assets/images/coin-5.png" alt="">
+            <img class="w-[32px] aspect-square min-[768px]:w-[40px] min-[992px]:w-[60px] min-[1200px]:w-[80px]"
+                 src="../assets/images/coin-6.png" alt="">
+            <img class="w-[32px] aspect-square min-[768px]:w-[40px] min-[992px]:w-[60px] min-[1200px]:w-[80px]"
+                 src="../assets/images/coin-7.png" alt="">
+            <img class="w-[32px] aspect-square min-[768px]:w-[40px] min-[992px]:w-[60px] min-[1200px]:w-[80px]"
+                 src="../assets/images/coin-8.png" alt="">
+          </div>
+        </div>
+        <div
+            class="h-[168px] flex items-center p-[12px] bg-[#FF4800] text-white font-[300] text-[14px] leading-[24px]
+                 min-[576px]:h-[120px] min-[768px]:h-auto min-[768px]:max-w-[628px]
+                 min-[992px]:text-[18px] min-[992px]:p-[24px] min-[992px]:leading-[30px]"
+        >
+          Lion isn't your ordinary meme coin - far from it?
+          <br>
+          We are the Alpha Predator meme coin, the hunters prowling the
+          digital jungle for weak hands. Those who can't grip their money tight, beware! Lion is on the hunt, ready to
+          separate the paper-hand guys from the true alpha holders. Join us in the chase for financial dominance!
+        </div>
+      </div>
+    </div>
+    <div class="relative min-[768px]:flex justify-between">
+      <div
+          class="max-[768px]:m-auto max-[768px]:max-w-[540px] max-[576px]:max-w-[343px] pt-[32px] flex flex-col
+                 min-[768px]:max-w-[55.5%] min-[768px]:pt-[60px] min-[992px]:pt-[72px]"
+      >
+        <div
+            class="flex flex-col items-center min-[768px]:pl-[20px] min-[768px]:items-start justify-start min-[992px]:pl-[80px]"
+        >
+          <button
+              class="py-[16px] px-[24px] text-center text-white font-[900] text-[32px] w-full bg-[#FF4800] leading-[40px]
+                     min-[768px]:max-w-[230px] min-[992px]:text-[72px] min-[992px]:leading-[90px] min-[992px]:max-w-fit"
+          >
+            What is Lion?
+          </button>
+          <h2
+              class="text-2xl font-[900] mt-[24px] text-[#1E293B] tracking-[0.9px] self-start min-[992px]:text-4xl
+                     min-[1200px]:text-[48px] min-[1200px]:leading-[68px]"
+          >
+            The Alpha Predator meme coin,
+            <br>
+            The King of all Meme Coin
+          </h2>
+          <p
+              class="pt-[12px] text-[14px] font-[300] leading-[24px] text-[#475569] min-[992px]:pt-[24px] min-[992px]:text-[18px]
+                     min-[992px]:leading-[30px]"
+          >
+            $LION wants to overcome the limitations of existing DogeShibaInuPepeBabyDogeBone coins. While these coins
+            have achieved remarkable success, they still face certain constraints that we, at Lion, are determined to
+            improve upon! $LION is here to fix the past and pounce into the future.
+            <br>
+            Why settle for snooze-worthy meme coins when you can blast off with Lion?
+          </p>
+        </div>
+      </div>
+      <div class="relative flex justify-end mt-[24px] overflow-hidden flex-shrink-0">
+        <p
+            class="absolute text-2xl max-w-[195px] top-[72px] right-0 min-[992px]:text-[32px] font-[900]
+                   min-[992px]:leading-[40px] min-[992px]:max-w-[212px] min-[992px]:top-[108px] min-[992px]:right-[18px]"
+        >
+          Long live the King
+          King of Meme coin
+        </p>
+        <div class="absolute right-[100px] top-[140px] min-[992px]:right-[120px] min-[992px]:top-[200px]">
+          <svg
+              class="w-[75px] h-[95px] "
+              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 79 100" fill="none">
+            <path
+                d="M2 97.9952C10.5689 96.1298 16.6258 91.8638 23.3493 86.3238C37.025 75.0554 50.264 63.1612 59.9311 48.1404C67.2191 36.8162 73.8491 23.5238 76.5607 10.2126C77.1105 7.51348 77.4467 4.75654 77.4467 2"
+                stroke="#334155" stroke-width="3" stroke-linecap="round"/>
+          </svg>
+        </div>
+        <img
+            class="h-[523px] min-[992px]:h-[629px] flex-shrink-0"
+            src="../assets/images/monkey-hover-coin.jpg"
+            alt=""
+        >
+      </div>
+    </div>
+    <div class="bg-primary">
+      <div class="container py-[32px]">
+        <div
+            class="py-[16px] px-[24px] text-center text-white font-[900] text-[32px] w-full bg-[#FF4800] leading-[40px]"
+        >
+          Getting Started
+        </div>
+        <div>
+          <div class="pt-[12px] text-white p-[12px] border-[#1E293B] border-t-0 border-[2px] flex flex-col gap-[12px]">
+            <h2 class="text-[32px] leading-[40px] font-[900]">1_</h2>
+            <h2 class="text-[24px] text-[#FF4800 font-[900] leading-[32px] text-[#FF4800]">Choose your wallet</h2>
+            <p class="text-[14px] font-[300] leading-[24px]">
+              A wallet is necessary for people wanting to use, trade, or hold $LION Tokens. Make sure you have an
+              ERC20-compatible wallet, such as MetaMask or Trust Wallet, and connect it to the Ethereum Main
+              Network.
+            </p>
+            <div class="flex gap-[12px] mt-[12px] justify-center">
+              <img class="w-[68px] aspect-square" src="../assets/images/wallet1.png" alt="">
+              <img class="w-[68px] aspect-square" src="../assets/images/wallet2.png" alt="">
+              <img class="w-[68px] aspect-square" src="../assets/images/wallet3.png" alt="">
+              <img class="w-[68px] aspect-square" src="../assets/images/wallet4.png" alt="">
+            </div>
+          </div>
+          <div class="pt-[12px] text-white p-[12px] border-[#1E293B] border-t-0 border-[2px] flex flex-col gap-[12px]">
+            <h2 class="text-[32px] leading-[40px] font-[900]">2_</h2>
+            <h2 class="text-[24px] text-[#FF4800 font-[900] leading-[32px] text-[#FF4800]">Get some ETH</h2>
+            <p class="text-[14px] font-[300] leading-[24px]">
+              Have ETH in your wallet to switch to $LION. If you don’t have any ETH, you can buy directly on Metamask or
+              Trust, transfer from another wallet, or buy on another exchange and send it to your ERC20-compatible
+              wallet.
+            </p>
+            <div class="flex gap-[12px] mt-[12px] justify-center">
+              <img class="w-[68px] aspect-square" src="../assets/images/ETH.png" alt="">
+            </div>
+          </div>
+          <div class="pt-[12px] text-white p-[12px] border-[#1E293B] border-t-0 border-[2px] flex flex-col gap-[12px]">
+            <h2 class="text-[32px] leading-[40px] font-[900]">3_</h2>
+            <h2 class="text-[24px] text-[#FF4800 font-[900] leading-[32px] text-[#FF4800]">
+              Access Uniswap
+            </h2>
+            <p class="text-[14px] font-[300] leading-[24px]">
+              Connect your wallet to Uniswap. Paste the $LION token address into Uniswap, select LION, and confirm.
+            </p>
+            <div class="flex gap-[12px] mt-[12px] justify-center">
+              <img class="w-[68px] aspect-square" src="../assets/images/wallet-uniswap.png" alt="">
+            </div>
+          </div>
+          <div class="pt-[12px] text-white p-[12px] border-[#1E293B] border-t-0 border-[2px] flex flex-col gap-[12px]">
+            <h2 class="text-[32px] leading-[40px] font-[900]">4_</h2>
+            <h2 class="text-[24px] text-[#FF4800 font-[900] leading-[32px] text-[#FF4800]">
+              Swap ETH for $LION
+
+            </h2>
+            <p class="text-[14px] font-[300] leading-[24px]">
+              Enter the amount of ETH you want to swap for $LION or use the "Max" button to swap all your ETH. You may
+              need to use slippage during times of market volatility.
+            </p>
+            <div class="flex gap-[8px] mt-[12px] justify-center items-center">
+              <img class="w-[68px] aspect-square" src="../assets/images/ETH.png" alt="">
+              <img class="w-[24px] aspect-square" src="../assets/images/arrow-right-line.png" alt="">
+              <img class="w-[68px] aspect-square" src="../assets/images/lion-coin.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="relative flex flex-col items-center">
+          <img class="h-[150px]" src="../assets/images/lion-relax.png" alt="">
+          <div class="relative">
+            <svg class="absolute bottom-[-25px] left-[-65px]" xmlns="http://www.w3.org/2000/svg" width="11" height="83"
+                 viewBox="0 0 11 83" fill="none">
+              <path d="M7.16475 2.4198C2.35376 28.3992 -1.63631 56.2326 9.41255 81.0925" stroke="white" stroke-width="3"
+                    stroke-linecap="round"/>
+            </svg>
+          </div>
+          <h2 class="text-center text-[24px] font-[900] text-white leading-[32px] mt-[37px]">
+            Relax my Lion Legion
+            <br>
+            We'll get that money anyway
+          </h2>
+          <h2 class="text-[24px] leading-[32px] font-[900] text-center text-white mt-[48px]">
+            Follow the steps above and go get yourself a money-counting machine!
+          </h2>
+          <div class="relative z-30">
+            <svg class="absolute top-[8px] left-[-80px]" xmlns="http://www.w3.org/2000/svg" width="63" height="52"
+                 viewBox="0 0 63 52"
+                 fill="none">
+              <path
+                  d="M1.82587 49.5394C2.61784 48.3515 5.39005 48.0711 6.54122 47.6238C15.8266 44.0161 24.7636 38.4792 32.8809 32.7778C43.5024 25.3174 55.8921 14.833 60.8414 2.45959"
+                  stroke="white" stroke-width="3" stroke-linecap="round"/>
+            </svg>
+          </div>
+          <img class="mt-[26px] self-start z-10" src="../assets/images/meerkat.png" alt="">
+          <div class="relative"><h2 class="absolute">Lion Coin</h2></div>
+        </div>
       </div>
     </div>
   </div>
@@ -36,5 +238,17 @@
   background-position-x: 50%;
   display: flex;
   flex-direction: column;
+  padding-top: 32px;
+  justify-content: space-between;
+}
+
+.bg-primary {
+  background: linear-gradient(0deg, #0F172A 0%, #0F172A) 100%, #FFFCFB;
+}
+
+@media (min-width: 992px) {
+  .banner-wrap {
+    height: 1010px;
+  }
 }
 </style>
